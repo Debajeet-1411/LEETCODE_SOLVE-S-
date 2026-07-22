92c2c4e5dd692af84a60445d0082d0e798d97d2d
Debajeet Mandal <ninjarusher1411@gmail.com>
2026-07-22 14:38:18 +0530

    Time: 4 ms (16.83%), Space: 15 MB (17.34%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> freq;
+        for(int i =0; i < nums1.size(); i++){
+            if(freq.find(nums1[i]) == freq.end()){
+                freq[nums1[i]] = 1;
+            }
+            else if (freq.find(nums1[i]) != freq.end()){
+                freq[nums1[i]] += 1;
+            }
+        }
+    // created the hash map now need to checkk the existance
+    vector<int> ans;
+    for (auto x : nums2){
+        if(freq[x] > 0){
+            ans.push_back(x);
+            freq[x] -= 1;
+        }
+    }
+    return ans;
+
+    }
+};
