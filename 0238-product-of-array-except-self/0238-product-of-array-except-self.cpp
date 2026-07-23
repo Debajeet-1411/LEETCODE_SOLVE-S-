38cc98f87c0314ee293fe134020580e8391d9316
Debajeet Mandal <ninjarusher1411@gmail.com>
2026-07-23 17:40:42 +0530

    Time: 2 ms (43.21%), Space: 44.6 MB (5.07%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int pr = 1;
+        unordered_set<int> seq;
+        vector<int> product;
+        unordered_map<int,int> zeros;
+        
+        for (auto x : nums){
+            seq.insert(x);
+            if (x != 0){
+            pr *= x;
+            product.push_back(pr);
+            }
+            else{
+                zeros[x]++;
+            }
+        }
+
+    //i have the set to find weteher 0 is there in the vector
+    //i have the multiplicationn vector too
+    vector<int> ans;
+    if(zeros[0]>1){
+                ans.resize(nums.size(),0);
+                return ans;
+            }
+    
+    if(seq.find(0) == seq.end()){
+        for (auto x: nums){
+            int n = product[product.size() - 1] / x;
+            ans.push_back(n);
+        }
+    }
+    else if (seq.find(0) != seq.end()){
+        for (auto x : nums){
+            if (x != 0){
+                ans.push_back(0);
+            }
+            else{
+                int n = product[product.size() - 1];
+                ans.push_back(n);
+            }
+        }
+    }      
+
+
+        return ans;
+    }
+};
