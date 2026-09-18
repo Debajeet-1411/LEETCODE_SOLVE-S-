686e793f4c87ccee8fa05aca0f8343754dad1fc3
Debajeet Mandal <ninjarusher1411@gmail.com>
2026-09-18 17:09:45 +0530

    Time: 12 ms (82.97%), Space: 26.2 MB (32.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,19 +3,16 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
         unordered_map<string, vector<string>> mp;
-        for (string words : strs){
-            string key = words;
-            sort(key.begin(), key.end());
-            mp[key].push_back(words);
+
+        for(auto x : strs){
+            string word = x;
+            sort(word.begin(), word.end());
+            mp[word].push_back(x);
         }
 
-        for(auto& x: mp){
-            ans.push_back(x.second);
+        for(auto i : mp){
+            ans.push_back(i.second);
         }
-
-
-
-
         return ans;
     }
 };
